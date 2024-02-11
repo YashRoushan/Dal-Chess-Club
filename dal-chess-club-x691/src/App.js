77f1b53963eca7a2ad23748516f5a7a1bd7ac36c
davidweji <130021498+davidweji@users.noreply.github.com>
2024-02-11 13:40:00 -0400

    My redo since I didn't git pull before my last push, when I went to try and git pull some issues came up so I just re-did my branch and now I have the develop changes as well as my code

--- dal-chess-club-x691/src/App.js
+++ dal-chess-club-x691/src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Tournaments from './pages/tournaments.js';
 import News from './pages/News';
+import NavBar from './pages/navbar';
+import Footer from './pages/footer';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
     <Router>
+        <NavBar />
         <Routes>
           <Route path="/tournaments" element={<Tournaments />} />
           <Route path="/news" element={<News />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
