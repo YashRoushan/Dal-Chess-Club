389def4bc298f97d6a5697e893f9391fab3ba5e4
Natalie <nt479065@dal.ca>
2024-03-15 09:52:41 -0300

    APIs working. Establishing an SSH connection so iw works from all locations.

--- dal-chess-club-x691/src/server.js
+++ dal-chess-club-x691/src/server.js
@@ -1,33 +1,41 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const mysql = require('mysql');
+const { Client } = require('ssh2');
+const sshClient = new Client();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const mysql = require('mysql');
+
+const sshConfig = {
+  host: 'euro.cs.dal.ca:22',
+  port: 22, // Standard SSH port
+  username: 'chessclub',
+  privateKey: "C:\Users\natal\.ssh\id_rsa",
+  password: 'eshooRi9oofaVah0',
+};
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "euro.cs.dal.ca",
+  user: "chessclub",
+  password: "Mee5shaong9kaiw4",
+  database: "chessclub",
+  port: 3306,
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
-});
 
-db.connect((err) => {
+
+/*db.connect((err) => {
   if (err) {
     console.error('Error connecting to the database: ', err);
     return;
   }
   console.log('Connected to the database');
-});
+});*/
 
-/*
 app.get('/api/data', async (req, res) => {
     try {
       const [rows] = await require('./database').query('SELECT * FROM my_table');
@@ -35,22 +43,23 @@ app.get('/api/data', async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  });*/
+  });
 
   //REST API for displaying tournaments on tournaments page
-  app.get("/tournaments", (req,res) => {
-    const query = "SELECT * FROM tournaments"
-    pool.query(query, (err,data) => {
-      if (err) return res.json(err)
-      return res.json(data)
-    })
-  })
+  app.get("/tournaments", (req, res) => {
+    const tournamentQuery = "SELECT * FROM tournaments";
+    db.query(tournamentQuery, (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+});
+
 
   app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
   
-  /*
-  const PORT = process.env.PORT || 3001;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));*/
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
