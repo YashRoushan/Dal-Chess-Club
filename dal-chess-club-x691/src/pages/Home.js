3a51f9216eb0e0a0dfc9a17bf0358c0b12210422
krana <kh958419@dal.ca>
2024-04-02 16:54:37 -0300

    apis and logo code

--- dal-chess-club-x691/src/pages/Home.js
+++ dal-chess-club-x691/src/pages/Home.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { Container, Grid, Card, CardActionArea, CardMedia, CardContent, Typography } from "@mui/material";
@@ -8,149 +9,104 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/navigation";
-import slide1Image1 from "../images/slide-1.jpg";
-import slide1Image2 from "../images/slide-2.jpeg";
-import slide1Image3 from "../images/slide-3.jpg";
 
-import slide1Image11 from "../images/slide-1.jpg";
-import slide1Image12 from "../images/slide-2.jpeg";
-import slide1Image13 from "../images/slide-3.jpg";
 import '../styles/homestyle.css'
 import ChessPuzzle from "./chessPuzzle";
 
 function Home() {
+  const [slides, setslides] = useState([]);
+  const [cards, setcards] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/home/getHomePageSlides`)
+      .then(res => res.json())
+      .then(data => {       
+        setslides(data);
+      })
+      .catch(err => console.log(err))
+  }, []);
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/home/homePageCards`)
+      .then(res => res.json())
+      .then(data => setcards(data))
+      .catch(err => console.log(err))
+  }, []);
   return (
     <>
-    <div className="homeTitle">
-    <h1>Dalhousie Chess Club</h1>
-    </div>
+      <div className="homeTitle">
+        <h1>Dalhousie Chess Club</h1>
+      </div>
       {/* swiper-slider container */}
       <div className="sliderMain">
-      <Swiper
-  spaceBetween={0}
-  hashNavigation={{
-    watchState: true,
-  }}
-  pagination={{
-    clickable: true,
-  }}
-  navigation={true}
-  modules={[Pagination, Navigation, HashNavigation, Autoplay]} 
-  autoplay={{
-    delay: 4000,  // Adjust the delay as needed
-    disableOnInteraction: false,
-  }}
-  loop={true}
-  className="mySwiper"
->
-          <SwiperSlide data-hash="slide1">
+        <Swiper
+          spaceBetween={0}
+          hashNavigation={{
+            watchState: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation, HashNavigation, Autoplay]}
+          autoplay={{
+            delay: 4000,  
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          className="mySwiper"
+        >
+          {slides.map((data, index) => (
+            <SwiperSlide key={index} data-hash={`data${index + 1}`}>
 
-            <div className="innerContent">
-              <Link to='/' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}}><img src={slide1Image1} alt="drone view of dalhousie campus"/></Link>
-              <div>
-              <h1>WELCOME TO THE DAL CHESS CLUB</h1>
-              <p>
-                Bringing Together Players at Dalhousie University
-              </p>
+              <div className="innerContent">
+                <Link to='/' onClick={() => { window.scroll({ top: 0, left: 0, behavior: "smooth", }); }}><img src={data.image} alt={data.title} /></Link>
+                <div>
+                  <h1>{data.title}</h1>
+                  <p>
+                    {data.description}
+                  </p>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide data-hash="slide2">
-          <div className="innerContent">
-              <Link to='/news' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}}><img src={slide1Image2} alt="chess board"/></Link>
-              <div>
-              <h1>CHESS FEST</h1>
-              <p>
-              Our Recent Seminar on Strategies was a Great Success
-              </p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide data-hash="slide3">
-          <div className="innerContent">
-              <Link to='/tournaments' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}}><img src={slide1Image3} alt="chess club kiosk"/></Link>
-              <div>
-              <h1>UPCOMING TOURNAMENTS</h1>
-              <p>
-              Follow Us on Social Media to Learn More About Upcoming Events
-              </p>
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
 
 
       {/* cards-material ui */}
       <Container className="my-5">
-        <Grid container spacing={3} className="cardCenter">
-          <Grid item md={4} className="mt-3">
+      <Grid container spacing={3} className="cardCenter">
+        {cards.map((card, index) => (
+          <Grid item md={4} className="mt-3" key={index}>
             <Card>
-              <CardActionArea id="cardHover" component={NavLink} to="/news" onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}}>
-                <CardMedia id="cardimg"
-                  component="img"
-                  alt="WEEKLY MEETINGS"
-                  height="235"
-                  image={slide1Image11}
-                  title="WEEKLY MEETINGS"
-                />
-              </CardActionArea>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  WEEKLY MEETINGS
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Join Us on Mondays and Thursdays from 5:30 to 8:30
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item md={4} className="mt-3">
-            <Card>
-              <CardActionArea id="cardHover" component={NavLink} to="/about-us" onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}}>
+              <CardActionArea
+                id="cardHover"
+                component={NavLink}
+                to={card.link} // Assuming each card object has a 'link' property
+                onClick={() => { window.scroll({ top: 0, left: 0, behavior: "smooth" }); }}
+              >
                 <CardMedia
                   id="cardimg"
                   component="img"
-                  alt="MEET OUR TEAM"
+                  alt={card.title}
                   height="235"
-                  image={slide1Image12}
-                  title="MEET OUR TEAM"
+                  image={card.image}
+                  title={card.title}
                 />
               </CardActionArea>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  MEET OUR TEAM
+                  {card.title}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Learn More About our 2024 Team Members
+                  {card.description}
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item md={4} className="mt-3">
-            <Card>
-              <CardActionArea id="cardHover" component={NavLink} to="/faq" onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}}>
-                <CardMedia
-                  id="cardimg"
-                  component="img"
-                  alt="FAQ PAGE"
-                  height="235"
-                  image={slide1Image13}
-                  title="FAQ PAGE"
-                />
-              </CardActionArea>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  FAQ PAGE
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Get Answers to Frequently Asked Questions
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </Container>
+        ))}
+      </Grid>
+    </Container>
 
       <h1>Daily Puzzle</h1>
       <ChessPuzzle />
