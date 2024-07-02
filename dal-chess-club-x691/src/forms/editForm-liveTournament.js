12d90d973377859a7ac5ea8af4b798e401e5d967
yash roushan <yashroushan@gmail.com>
2024-07-02 16:38:37 -0300

    added functionality for the admin to post iframes to show live tournament updates

--- dal-chess-club-x691/src/forms/editForm-liveTournament.js
+++ dal-chess-club-x691/src/forms/editForm-liveTournament.js
@@ -4,16 +4,8 @@ import { BASE_URL } from "../config";
 
 function LiveTournamentsAddForm() {
     const parameters = new URLSearchParams(window.location.search);
-    // console.log(parameters.get("itemId"));
     const currentGameId = parameters.get('itemId');
     const [gameId, setGameId] = useState(currentGameId);
-    const [Player1, setPlayer1] = useState('');
-    const [Player2, setPlayer2] = useState('');
-    const [Player1_time, setPlayer1_time] = useState('');
-    const [Player2_time, setPlayer2_time] = useState('');
-    const [Player1_score, setPlayer1_score] = useState(0);
-    const [Player2_score, setPlayer2_score] = useState(0);
-    const [game_date, setGame_date] = useState('');
     const [pairingsIframe, setPairingsIframe] = useState('');
     const [standingsIframe, setStandingsIframe] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
@@ -32,13 +24,8 @@ function LiveTournamentsAddForm() {
                 // Show the default page.
             }
             response.json().then(data => {
-                setPlayer1(data.Player1);
-                setPlayer2(data.Player2);
-                setPlayer1_time(data.Player1_time);
-                setPlayer2_time(data.Player2_time);
-                setPlayer1_score(data.Player1_score)
-                setPlayer2_score(data.Player2_score);
-                data.game_date !== undefined && setGame_date(new Date(data.game_date).toISOString().slice(0, 19).replace('T', ' '));
+                setStandingsIframe(data.Standings);
+                setPairingsIframe(data.Pairings);
             })
         })
     }
@@ -46,7 +33,9 @@ function LiveTournamentsAddForm() {
 
     const handleAdd = async (event) => {
         event.preventDefault();
-        const formData = { gameId, Player1, Player2, Player1_time, Player2_time, Player1_score, Player2_score, game_date };
+        const Pairings = pairingsIframe;
+        const Standings = standingsIframe;
+        const formData = { gameId, Pairings, Standings };
         try {
             const response = await fetch(`${BASE_URL}/api/live-tournaments/edit/${gameId}`, {
                 method: 'PUT',
