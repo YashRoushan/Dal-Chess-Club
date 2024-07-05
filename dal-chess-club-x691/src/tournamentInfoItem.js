a783abe407009f6260e5c2b6e01468018c6b9a31
Ethan Yee <130222292+et802627@users.noreply.github.com>
2024-07-05 00:01:17 -0300

    Added the register button to the bottom of the tournament popup feature that is actually now a full page

--- dal-chess-club-x691/src/tournamentInfoItem.js
+++ dal-chess-club-x691/src/tournamentInfoItem.js
@@ -1,9 +1,17 @@
 import React, {useState} from "react";
 import "./styles/tournaments.css";
+import { useNavigate } from 'react-router-dom';
 
 function TournamentInfoItem({name, image, date, time, endTime, participantsNo, price, description, registrationLink }) {
     
   let embedLink = registrationLink + "&embed=true";
+
+  const navigate = useNavigate();
+
+  // Updated function to handle registration button click
+  const handleRegisterNow = (e) => {
+    navigate('/tournamentRegistration'); // Navigate to the registration page
+  }
     
   return (
     <div className='editPage-container'>
@@ -14,11 +22,14 @@ function TournamentInfoItem({name, image, date, time, endTime, participantsNo, p
         <p> Time: {time} - {endTime}</p>
         <p> Number of Participants: {participantsNo} </p>
         <p> {description} </p>
-        {registrationLink && (
+        {/* {registrationLink && (
         <a href={registrationLink} target="_blank" rel="noopener noreferrer">
           <button className="registerButton">Register</button>
         </a>
-      )}
+        )} */}
+        <div className="participants-register-container">
+          <button className="register-now-button" onClick={handleRegisterNow}>Register Now</button>
+        </div>
     </div>
   );
 }
