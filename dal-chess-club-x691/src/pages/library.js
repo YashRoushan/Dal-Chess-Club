1255932bbde77fd3fa4811e0d36e57e5f4496850
NguyenDal <annguyen270504@gmail.com>
2024-07-13 00:06:18 -0300

    Images to be fixed later

--- dal-chess-club-x691/src/pages/library.js
+++ dal-chess-club-x691/src/pages/library.js
@@ -1,27 +1,42 @@
-import React from 'react'
-import { libraryList } from '../libraryBooksList'
-import LibraryItem from '../libraryBooksItem'
-import '../styles/library.css'
+import React, { useState, useEffect } from 'react';
+import LibraryItem from '../libraryBooksItem';
+import '../styles/library.css';
+import { BASE_URL } from '../config.js'; 
 
-function library() {
-    return(
-        <div className="library">
-        <h1>Library Books List</h1>
-          <div className="libraryList">
-            
-              {libraryList.map((libraryItem, key) => {
-                return(
-                  <LibraryItem
-                    key={key}
-                    name={libraryItem.name}
-                    image={libraryItem.image}
-                    author={libraryItem.author}
-                  />
-                )
-              })}
-          </div>
+function Library() {
+  const [libraryList, setLibraryList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/api/library`) 
+      .then(response => response.json())
+      .then(data => {
+        console.log("Fetched data:", data); // Debug fetched data
+        setLibraryList(data); // Assuming the endpoint returns an array directly
+        data.forEach(item => console.log(`Image URL for ${item.title}: ${item.image}`)); // Debug image URLs
+        
+      })
+      .catch(error => console.error('Error fetching books:', error));
+  }, []);
+
+  return (
+    <div className="library">
+      <h1>Library Books List</h1>
+      <div className="libraryList">
+        {libraryList.map((libraryItem, index) => (
+          <LibraryItem
+            key={index}
+            id={libraryItem.booksID} 
+            name={libraryItem.title}
+            image={libraryItem.image}
+            author={libraryItem.author}
+            description={libraryItem.description} 
+          />
+        ))}
       </div>
-    )
+    </div>
+  );
 }
 
-export default library
+export default Library;
+
+
