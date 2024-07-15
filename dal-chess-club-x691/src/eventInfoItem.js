9d598833f0cf8cb4ed098d70784c59ba4177daad
Ethan Yee <130222292+et802627@users.noreply.github.com>
2024-07-15 12:15:00 -0300

    removed the tournament standings and pairings implementation

--- dal-chess-club-x691/src/eventInfoItem.js
+++ dal-chess-club-x691/src/eventInfoItem.js
@@ -4,9 +4,6 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { BASE_URL } from "./config";
 
 function EventInfoItem({ tournamentID, name, image, date, time, endTime, participantsNo, price, description, registrationLink, participants }) {
-  const navigate = useNavigate();
-  const [pairings, setPairings] = useState('');
-  const [standings, setStandings] = useState('');
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const itemId = searchParams.get('itemId');
@@ -24,8 +21,6 @@ function EventInfoItem({ tournamentID, name, image, date, time, endTime, partici
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setStandings(data.Standings);
-      setPairings(data.Pairings);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -61,12 +56,6 @@ function EventInfoItem({ tournamentID, name, image, date, time, endTime, partici
             <button className="tregisterButton">Register</button>
           </a>
         )}
-        {/* <div className="ttables">
-          <h3>Standings</h3>
-          <div className="tstandings-table" dangerouslySetInnerHTML={{ __html: standings }}></div>
-          <h3>Pairings</h3>
-          <div className="tpairings-table" dangerouslySetInnerHTML={{ __html: pairings }}></div>
-        </div> */}
       </div>
     </div>
   );
