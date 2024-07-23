9a2ba2bfe51611273c13e21aa677a7e249232395
NguyenDal <annguyen270504@gmail.com>
2024-07-23 12:10:06 -0300

    Added sliders for easy browsing

--- dal-chess-club-x691/src/pages/dailyTips.js
+++ dal-chess-club-x691/src/pages/dailyTips.js
@@ -2,39 +2,70 @@ import React, { useState, useEffect } from 'react';
 import '../styles/dailyTips.css'; // Use dailyTips.css for styling
 import { BASE_URL } from '../config.js';
 
-const Tip = ({ title, type, typeIntro, description, image_link }) => {
+const Tip = ({ title, description, image_link }) => {
     const imageUrl = image_link ? `${BASE_URL}${image_link}` : '';
 
     return (
-        <div className="tip-article">
-            <div className="type-info">
-                <p className="type"><strong>{type}</strong> - {typeIntro}</p>
-            </div>
-            <h2>{title}</h2>
-            <div className="tip-content">
+        <div className="tip-content">
+            <div className="tip-text">
+                <h2>{title}</h2>
                 <p>{description}</p>
-                {imageUrl && <img className="tip-image" src={imageUrl} alt={title} />}
+            </div>
+            {imageUrl && <img src={imageUrl} alt={title} className="tip-image" />}
+        </div>
+    );
+};
+
+const TipSlider = ({ tips, type, typeIntro }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextTip = () => {
+        setCurrentIndex((currentIndex + 1) % tips.length);
+    };
+
+    const prevTip = () => {
+        setCurrentIndex((currentIndex - 1 + tips.length) % tips.length);
+    };
+
+    const handleDotClick = (index) => {
+        setCurrentIndex(index);
+    };
+
+    return (
+        <div className="tip-container">
+            <div className="type-intro-box">
+                <p><strong>{type}:</strong> {typeIntro}</p>
+            </div>
+            <div className="tip-slider-container">
+                <button onClick={prevTip} className="slide-btn prev-btn">{"<"}</button>
+                <div className="tip-content-wrapper">
+                    {tips.length > 0 && <Tip {...tips[currentIndex]} />}
+                </div>
+                <button onClick={nextTip} className="slide-btn next-btn">{">"}</button>
+            </div>
+            <div className="dot-container">
+                {tips.map((_, index) => (
+                    <span key={index} 
+                          onClick={() => handleDotClick(index)}
+                          className={`dot ${index === currentIndex ? 'active' : ''}`}></span>
+                ))}
             </div>
         </div>
     );
 };
 
 function DailyTips() {
-    const [openingTip, setOpeningTip] = useState(null);
-    const [middleGameTip, setMiddleGameTip] = useState(null);
-    const [endgameTip, setEndgameTip] = useState(null);
+    const [openingTips, setOpeningTips] = useState([]);
+    const [middleGameTips, setMiddleGameTips] = useState([]);
+    const [endgameTips, setEndgameTips] = useState([]);
 
     const fetchTips = async () => {
         try {
             const response = await fetch(`${BASE_URL}/api/tips`);
             const data = await response.json();
-            const openingTips = data.filter(tip => tip.type.toLowerCase() === 'opening');
-            const middleGameTips = data.filter(tip => tip.type.toLowerCase() === 'middle game');
-            const endgameTips = data.filter(tip => tip.type.toLowerCase() === 'endgame');
-
-            if (openingTips.length > 0) setOpeningTip(openingTips[Math.floor(Math.random() * openingTips.length)]);
-            if (middleGameTips.length > 0) setMiddleGameTip(middleGameTips[Math.floor(Math.random() * middleGameTips.length)]);
-            if (endgameTips.length > 0) setEndgameTip(endgameTips[Math.floor(Math.random() * endgameTips.length)]);
+            setOpeningTips(data.filter(tip => tip.type.toLowerCase() === 'opening'));
+            setMiddleGameTips(data.filter(tip => tip.type.toLowerCase() === 'middle game'));
+            setEndgameTips(data.filter(tip => tip.type.toLowerCase() === 'endgame'));
         } catch (error) {
             console.error('Error fetching tips:', error);
         }
@@ -46,20 +77,37 @@ function DailyTips() {
 
     return (
         <div className="daily-tips-page">
-            <h1 className="daily-tips-header">Daily Chess Tips</h1>
+            <h1 className="daily-tips-header">Chess Tips</h1>
             <p className="daily-tips-intro">Welcome to the Daily Chess Tips page! Here, you'll find valuable tips to improve your chess game.</p>
-            <p className="daily-tips-intro">Every time you visit, you'll receive a new tip for the opening, middle game, and endgame. Refresh the page to discover new strategies and enhance your skills.</p>
+            <p className="daily-tips-intro">Use the sliders below to browse through tips for the opening, middle game, and endgame. Discover new strategies and enhance your skills.</p>
             <div className="tips-container">
-                {openingTip && <Tip {...openingTip} typeIntro="Mastering the art of the opening is a vital first step towards chess mastery. A strong opening sets the tone for the entire game, helping you establish control, develop your pieces effectively, and ensure the safety of your king." />}
-                {middleGameTip && <Tip {...middleGameTip} typeIntro="Transitioning to the middle game, the real strategic battle unfolds. It's not just about moving pieces; it's about launching a well-coordinated attack while maintaining a solid defense." />}
-                {endgameTip && <Tip {...endgameTip} typeIntro="The endgame is where chess matches are won or lost. It's a high-stakes phase where every move can make the difference between a resounding victory or a bitter defeat." />}
+                {openingTips.length > 0 && (
+                    <TipSlider 
+                        tips={openingTips}
+                        type="Opening"
+                        typeIntro="Mastering the art of the opening is a vital first step towards chess mastery. A strong opening sets the tone for the entire game, helping you establish control, develop your pieces effectively, and ensure the safety of your king."
+                    />
+                )}
+                {middleGameTips.length > 0 && (
+                    <TipSlider 
+                        tips={middleGameTips}
+                        type="Middle Game"
+                        typeIntro="Transitioning to the middle game, the real strategic battle unfolds. It's not just about moving pieces; it's about launching a well-coordinated attack while maintaining a solid defense."
+                    />
+                )}
+                {endgameTips.length > 0 && (
+                    <TipSlider 
+                        tips={endgameTips}
+                        type="Endgame"
+                        typeIntro="The endgame is where chess matches are won or lost. It's a high-stakes phase where every move can make the difference between a resounding victory or a bitter defeat."
+                    />
+                )}
             </div>
         </div>
     );
 }
 
 export default DailyTips;
-
 
 
 
