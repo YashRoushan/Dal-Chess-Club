65c35af7cce3e62197224c66986ec49b7615e25c
Param Patel <pr574132@dal.ca>
2024-07-27 02:14:36 -0300

    Added a section in Add and Edit form.

--- dal-chess-club-x691/src/forms/Champions-EditForm.js
+++ dal-chess-club-x691/src/forms/Champions-EditForm.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './AddForms.css';
 import { useLocation } from 'react-router-dom';
-import {BASE_URL} from "../config";
-import { NavigateBefore } from '@mui/icons-material';
+import { BASE_URL } from "../config";
+
+const tournaments = [
+  "Dalhousie Chess Championship",
+  "Dalhousie Open",
+  "Summer Rapid Open",
+  "Summer Blitz Battle",
+  "Owen Maitzen Rapid Open",
+  "Fall Blitz Battle",
+  "Winter Rapid Open",
+  "Winter Blitz Battle"
+];
 
 function ChampionsEditForm() {
   const location = useLocation();
@@ -12,6 +22,7 @@ function ChampionsEditForm() {
   const [successMessage, setSuccessMessage] = useState('');
   const [championName, setChampionName] = useState('');
   const [championYear, setChampionYear] = useState(new Date().toISOString().split('T')[0]);
+  const [tournament, setTournament] = useState(tournaments[0]);
 
   useEffect(() => {
     const fetchChampion = async () => {
@@ -26,6 +37,7 @@ function ChampionsEditForm() {
           setChampionName(result[0].name);
           const formattedYear = result[0].year.toString().split('T')[0];
           setChampionYear(formattedYear);
+          setTournament(result[0].tournament);
           setSuccessMessage(result[0].message);
         }
       } catch (error) {
@@ -37,7 +49,7 @@ function ChampionsEditForm() {
   }, [itemId]);
 
   const handleEdit = async () => {
-    const formData = { name: championName, year: championYear };
+    const formData = { name: championName, year: championYear, tournament: tournament };
     console.log(formData);
     try {
       const response = await fetch(`${BASE_URL}/api/champions/edit/${itemId}`, {
@@ -51,8 +63,10 @@ function ChampionsEditForm() {
       const result = await response.json();
       if (result) {
         console.log(result);
+        setSuccessMessage("Champion updated successfully");
       } else {
         console.error('Failed to update champion');
+        setSuccessMessage('Failed to update champion');
       }
     } catch (error) {
       console.error('Error updating champion:', error);
@@ -61,7 +75,6 @@ function ChampionsEditForm() {
 
   return (
     <div className="add-form-container">
-
       <div className="header-info">
         <h2 id="main-header">Champions Page Edit Form</h2>
         <p>This is the page where you, the admin, can edit existing champions in the "Champions" page.</p>
@@ -69,7 +82,7 @@ function ChampionsEditForm() {
 
       {successMessage && <div className="success-message">{successMessage}</div>}
       <div className="form-A">
-        <form className="form-element">
+        <div className="form-element">
           <label>Champion Name</label>
           <input 
             className="text-form" 
@@ -78,11 +91,11 @@ function ChampionsEditForm() {
             onChange={(e) => setChampionName(e.target.value)} 
             required 
           />
-        </form>
+        </div>
       </div>
 
       <div className="form-B">
-        <form className="form-element">
+        <div className="form-element">
           <label>Year</label>
           <input 
             className="date-form" 
@@ -91,13 +104,25 @@ function ChampionsEditForm() {
             onChange={(e) => setChampionYear(e.target.value)} 
             required 
           />
-        </form>
+        </div>
+        <div className="form-element">
+          <label>Tournament</label>
+          <select
+            className="dropdown-form"
+            value={tournament}
+            onChange={(e) => setTournament(e.target.value)}
+            required
+          >
+            {tournaments.map((tournament, index) => (
+              <option key={index} value={tournament}>{tournament}</option>
+            ))}
+          </select>
+        </div>
       </div>
 
       <div className="submit-button-container">
         <button onClick={handleEdit} type="submit">Submit</button>
       </div>
-
     </div>
   );
 }
